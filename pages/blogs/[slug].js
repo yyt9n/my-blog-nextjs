f7a311a012a2133c9a45ecf7e8557d3a989d20d7
yyt9n <h.yyt9n@gmail.com>
2022-10-23 08:24:42 +0900

    getStaticPaths

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,5 +1,5 @@
 import PageLayout from 'components/PageLayout';
-import { getBlogBySlug } from 'lib/api';
+import { getBlogBySlug, getAllBlogs } from 'lib/api';
 
 const BlogDetail = ({blog}) => {
   return (
@@ -9,10 +9,18 @@ const BlogDetail = ({blog}) => {
   )
 }
 
-export async function getServerSideProps({params}) {
+export async function getStaticProps({params}) {
   const blog = await getBlogBySlug(params.slug);
   return {
     props: {blog}
+  }
+}
+
+export async function getStaticPaths() {
+  const blogs = await getAllBlogs();
+  return {
+    paths: blogs?.map(b => ({params: {slug: b.slug}})),
+    fallback: false
   }
 }
 
