140b596494d37e7d8b55737e3ed0ae6fc2e98dc3
yyt9n <h.yyt9n@gmail.com>
2022-10-22 23:13:58 +0900

    google font + minor styling improvements

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -1,20 +1,26 @@
 import { Container } from 'react-bootstrap';
 import Navbar from './Navbar';
+import  Head  from 'next/head';
 
 export default function PageLayout({children, className}) {
   return (
-    <Container>
-      <Navbar />
-      <div className={`page-wrapper ${className}`}>
-        {children}
-      </div>
-      <footer className="page-footer">
-        <div>
-          <a href="#">courses</a>{' | '}
-          <a href="#">github</a>{' | '}
-          <a href="#">facebook</a>
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <Container>
+        <Navbar />
+        <div className={`page-wrapper ${className}`}>
+          {children}
         </div>
-      </footer>
-    </Container>
+        <footer className="page-footer">
+          <div>
+            <a href="#">courses</a>{' | '}
+            <a href="#">github</a>{' | '}
+            <a href="#">facebook</a>
+          </div>
+        </footer>
+      </Container>
+    </>
   )
 }
